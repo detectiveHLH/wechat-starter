885f72231ef17e668795ad05ab5048e6fd5742fd
hlh <545759184@qq.com>
2018-04-19 14:26:54 +0800

    Reset ip and appid

--- src/config/app.js
+++ src/config/app.js
@@ -1,4 +1,4 @@
-const IP = '192.168.199.222';
+const IP = '你自己的IP地址';
 
 // 应用配置
 export default {
@@ -9,7 +9,7 @@ export default {
   // 前端地址
   HOST_NAME_FRONT_END: process.env.NODE_ENV === 'development' ? `http://${IP}:8000` : '',
   // 开发者id
-  APP_ID: process.env.NODE_ENV === 'development' ? 'wx3b96ced225ff8eb0' : '',
+  APP_ID: process.env.NODE_ENV === 'development' ? '你自己的APP_ID' : '',
   // 应用授权作用域
   SCOPE: 'snsapi_base',
 };
